45956231aba80e7e6d6031201f34541d230760d0
imshehab <shehabshawki@gmail.com>
2020-02-07 19:12:03 +0200

    first-file

---
+++ RTOS_PORTING/Debug/makedep.mk
@@ -0,0 +1,22 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+croutine.c
+
+event_groups.c
+
+heap_2.c
+
+list.c
+
+port.c
+
+queue.c
+
+RTOS_PORTING.c
+
+tasks.c
+
+timers.c
+
---
+++ RTOS_PORTING/RTOS_PORTING.c
@@ -0,0 +1,36 @@
+﻿/*
+ * Toggle.c
+ *
+ * Created: 06/02/2020 07:49:34 م
+ *  Author: sheha
+ */ 
+
+/*
+ * Toggle_led.c
+ *
+ * Created: 06/02/2020 07:11:45 م
+ *  Author: sheha
+ */ 
+
+#define F_CPU	16000000
+#include <avr/io.h>
+#include "FreeRTOS.h"
+#include "task.h"
+
+void task(void *p){
+	
+	 DDRC |= (1<<7);
+
+	while (1)
+	{
+		PORTC ^= (1<<7);
+		vTaskDelay (1000/portTICK_PERIOD_MS);
+	}
+}
+int main(void)
+{
+             xTaskCreate(task,"led",200,NULL,1,NULL);
+ //  xTaskCreate(task,"firsttask",250,NULL,1,NULL);
+           vTaskStartScheduler();
+		  return 0;
+}
